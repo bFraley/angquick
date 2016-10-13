31aca17c39e7fecae10bf264bca90c6a81391848
bFraley <bFraley@users.noreply.github.com>
2016-10-13 01:26:22 -0400

    adds AngQuick class to lib.py, and start() function in main.py

--- lib.py
+++ lib.py
@@ -1,12 +1,40 @@
 import os
 
-class Check():
+class AngQuick():
     def __init__(self):
+        self.aq_text = 'angquick: '
         self.osname = os.name
 
-    def get_osname(self):
+
+    def check_posix(self):
         if not self.osname == 'posix':
             print('angquick currently only supports posix operating systems.\n')
             exit(1)
         else:
-            return self.osname
+            print('\n{} {}'.format(self.aq_text, self.osname))
+            return True
+
+    def try_open_read(self, filename):
+
+        try:
+            fp = open(filename)
+        except PermissionError:
+            return "some default data"
+        else:
+            with fp:
+                return fp.readlines()
+
+    def prompt(self):
+        newang_component = input('Angquick - New component name: ')
+        print('prompt called')
+        return newang_component
+
+
+
+# NOTES
+
+"""
+os.access(path, mode, *, dir_fd=None, effective_ids=False, follow_symlinks=True)
+"""
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from lib import AngQuick
+
+def start():
+    aq = AngQuick()
+    aq.check_posix()
+    aq.prompt()
+
+
+
+# call start()
+start()
