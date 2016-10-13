e13d3c4414231d3effb1dc404c70a39ed488f7b3
bFraley <bFraley@users.noreply.github.com>
2016-10-13 02:19:00 -0400

    Adds main feature: Angular component file generation.

--- lib.py
+++ lib.py
@@ -1,9 +1,19 @@
+# angquick - Copyright by Brett Fraley 2016
+# https://github.com/bFraley/angquick
+
 import os
 
 class AngQuick():
     def __init__(self):
         self.aq_text = 'angquick: '
         self.osname = os.name
+
+
+    def call(self, shell_string):
+        if os.system(shell_string):
+            return True
+        else:
+            return False
 
 
     def check_posix(self):
@@ -14,21 +24,36 @@ class AngQuick():
             print('\n{} {}'.format(self.aq_text, self.osname))
             return True
 
+
     def try_open_read(self, filename):
 
         try:
             fp = open(filename)
         except PermissionError:
-            return "some default data"
+            return "file permission error"
         else:
             with fp:
                 return fp.readlines()
+
 
     def prompt(self):
         newang_component = input('Angquick - New component name: ')
         print('prompt called')
         return newang_component
 
+
+    def gen_new_component_files(self, name):
+        module_path = 'client/' + name
+        touch = 'touch '
+        slash = '/'
+        command_string = touch + module_path + slash + name
+
+        os.mkdir(module_path)
+        
+        self.call(command_string + '.module.js')
+        self.call(command_string + '-page.component.js')
+        self.call(command_string + '-page.controller.js')
+        self.call(command_string + '-page.html')
 
 
 # NOTES
--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from lib import AngQuick
 def start():
     aq = AngQuick()
     aq.check_posix()
-    aq.prompt()
-
-
+    component_name = aq.prompt()
+    aq.gen_new_component_files(component_name)
 
 # call start()
 start()
