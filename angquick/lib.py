5331c93b55a3971243d8f3f2452173663d71d538
bFraley <bFraley@users.noreply.github.com>
2016-10-14 00:38:10 -0400

    accepts input from argv and generates a module dir and inner files

--- angquick/lib.py
+++ angquick/lib.py
@@ -1,4 +1,4 @@
-# angquick - Copyright by Brett Fraley 2016
+# angquick - lib.py
 # https://github.com/bFraley/angquick
 
 import os
@@ -6,6 +6,12 @@ import os
 class AngQuick():
     def __init__(self):
         self.osname = os.name
+        self.file_extensions = [
+            '.module.js',
+            '.component.js',
+            '.controller.js',
+            '.html'
+        ]
 
     # Execute shell commands.
     def call(self, shell_string):
@@ -20,20 +26,12 @@ class AngQuick():
             print('angquick: posix OS required.\n')
             exit(1)
 
-    def prompt(self):
-        newang_component = input('Angquick - New module name: ')
-        print('prompt called')
-        return newang_component
-
     def gen_new_component_files(self, name):
         module_path = name
         touch = 'touch '
         command_string = touch + module_path + '/' + name
-
         os.mkdir(module_path)
+
+        for ext in self.file_extensions:
+            self.call(command_string + ext)
         
-        self.call(command_string + '.module.js')
-        self.call(command_string + '.component.js')
-        self.call(command_string + '.controller.js')
-        self.call(command_string + '.html')
- 
