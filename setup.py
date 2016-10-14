bf9c68640700c8444430ca20174930c33b636820
bFraley <bFraley@users.noreply.github.com>
2016-10-13 21:24:53 -0400

    completes setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'angquick',
+  packages = ['angquick'],
+  version = '1.0',
+  description = 'Quick AngularJS file and code creation',
+  author = 'Brett Fraley',
+  author_email = 'brettfraley@gmail.com',
+  url = 'https://github.com/bfraley/angquick',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  keywords = ['AngularJS', 'file template', 'Django/Angular'],
+  classifiers = [],
+)
